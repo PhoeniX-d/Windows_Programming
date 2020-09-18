158bdf8eebf4e74393321bb43e63057432ad87d1
PhoeniX-d <prnv24choudhary@gmail.com>
2020-09-18 21:01:17 +0530

    Updated

--- 32Bit/MyFirstWindow/MyFirstWindow.cpp
+++ 32Bit/MyFirstWindow/MyFirstWindow.cpp
@@ -1,82 +1,77 @@
-// headers
+// Headers
+#include<Windows.h>
 
-#include<windows.h>
-
-// global declaration
-
+// Global declation
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-// WinMain
-
+//WinMain()
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
 {
-	// Declarations
-	WNDCLASSEX wndclass;
+	//Variable declarations
+	WNDCLASSEX 	wndclass;
 	HWND hwnd;
 	MSG msg;
 	TCHAR szAppName[] = TEXT("My Application");
 	
-	// zero-out wndclass members
-	//memset((void*)&wndclass, NULL, sizeof(WNDCLASSEX));
-						//OR
-	ZeroMemory((void *)&wndclass, sizeof(WNDCLASSEX));
-	
-	// Initializations
-	
+	//ZeroOut Memory
+	memset((void *)&wndclass, NULL, sizeof(WNDCLASSEX));
+	/* OR
+	*	ZeroMemory((void*)&wndclass, sizeof(WNDCLASSEX));
+	*/
 	wndclass.cbSize = sizeof(WNDCLASSEX);
 	wndclass.style = CS_HREDRAW | CS_VREDRAW;
 	wndclass.cbClsExtra = 0;
 	wndclass.cbWndExtra = 0;
-	wndclass.lpfnWndProc = WndProc; // this will register our callback function to WindowsOs
+	wndclass.lpfnWndProc = WndProc;
 	wndclass.hInstance = hInstance;
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
 	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);//Getting object from Windows Stock i.e. HBRUSH here
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wndclass.lpszClassName = szAppName;
 	wndclass.lpszMenuName = NULL;
-	
-	// register above class to OS
+
+	//Register structure to OS
 	RegisterClassEx(&wndclass);
-	
-	// Create Windows
-	
-	hwnd = CreateWindow
-			(
-					szAppName,
-					TEXT("My Window"),
-					WS_OVERLAPPEDWINDOW,
-					CW_USEDEFAULT,
-					CW_USEDEFAULT,
-					CW_USEDEFAULT,
-					CW_USEDEFAULT,
-					NULL,
-					NULL,
-					hInstance,
-					NULL
-			);
-	
+
+	//Create Window
+	hwnd = CreateWindow(
+		szAppName,				 /* Name of Application */
+		TEXT("My First Window"), /* Name of Caption of window */
+		WS_OVERLAPPEDWINDOW,	 /* Style of window */
+		CW_USEDEFAULT,			 /* x coordinate of window - lefttop */
+		CW_USEDEFAULT,			 /* y coordinate of windows - lefttop */
+		CW_USEDEFAULT,			 /* width of window */
+		CW_USEDEFAULT,			 /* height of window */
+		NULL,					 /* Handle of Parent process (HW_DESKTOP)*/
+		NULL,					 /* Handle of Menu */
+		hInstance,				 /* Handle of window */
+		NULL					 /* Extra parameter for CreateWindow */
+	);
+
+	//Show window stored in memory
 	ShowWindow(hwnd, iCmdShow);
 	UpdateWindow(hwnd);
-	
-	// message loop
+
+	//Message loop
 	while(GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-	
+
 	return ((int)msg.wParam);
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam , LPARAM lParam)
 {
+	//Code
 	switch(iMsg)
 	{
-		case WM_DESTROY:
+		case WM_DESTROY :
 			PostQuitMessage(0);
 			break;
 	}
-	
+
 	return (DefWindowProc(hwnd, iMsg, wParam, lParam));
 }
--- 64Bit/MyFirstWindow/MyFirstWindow.cpp
+++ 64Bit/MyFirstWindow/MyFirstWindow.cpp
@@ -1,74 +1,77 @@
-// Header
-#include<windows.h>
+// Headers
+#include<Windows.h>
 
-// gloabal declarations
+// Global declation
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
-// WinMain()
+//WinMain()
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
 {
-	WNDCLASSEX wndclass;
+	//Variable declarations
+	WNDCLASSEX 	wndclass;
 	HWND hwnd;
 	MSG msg;
-	TCHAR szAppName[] = TEXT("MyApp");
-
-	memset((void*)&wndclass, NULL, sizeof(WNDCLASSEX));
-	// code
-	// initialization of WNDCLASSEX
-
+	TCHAR szAppName[] = TEXT("My Application");
+	
+	//ZeroOut Memory
+	memset((void *)&wndclass, NULL, sizeof(WNDCLASSEX));
+	/* OR
+	*	ZeroMemory((void*)&wndclass, sizeof(WNDCLASSEX));
+	*/
 	wndclass.cbSize = sizeof(WNDCLASSEX);
 	wndclass.style = CS_HREDRAW | CS_VREDRAW;
 	wndclass.cbClsExtra = 0;
 	wndclass.cbWndExtra = 0;
 	wndclass.lpfnWndProc = WndProc;
 	wndclass.hInstance = hInstance;
-	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wndclass.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wndclass.lpszClassName = szAppName;
 	wndclass.lpszMenuName = NULL;
-	wndclass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
-	//register above class
+	//Register structure to OS
 	RegisterClassEx(&wndclass);
 
-	// create window
-	hwnd = CreateWindow
-		(
-			szAppName,
-			TEXT("My Application"),
-			WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			NULL,
-			NULL,
-			hInstance,
-			NULL
-		);
+	//Create Window
+	hwnd = CreateWindow(
+		szAppName,				 /* Name of Application */
+		TEXT("My First Window"), /* Name of Caption of window */
+		WS_OVERLAPPEDWINDOW,	 /* Style of window */
+		CW_USEDEFAULT,			 /* x coordinate of window - lefttop */
+		CW_USEDEFAULT,			 /* y coordinate of windows - lefttop */
+		CW_USEDEFAULT,			 /* width of window */
+		CW_USEDEFAULT,			 /* height of window */
+		NULL,					 /* Handle of Parent process (HW_DESKTOP)*/
+		NULL,					 /* Handle of Menu */
+		hInstance,				 /* Handle of window */
+		NULL					 /* Extra parameter for CreateWindow */
+	);
+
+	//Show window stored in memory
 	ShowWindow(hwnd, iCmdShow);
 	UpdateWindow(hwnd);
-	
-	// message loop
-	while (GetMessage(&msg, NULL, 0, 0))
+
+	//Message loop
+	while(GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
 
-	return((int)msg.wParam);
+	return ((int)msg.wParam);
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam , LPARAM lParam)
 {
-	// code
-	switch (iMsg)
+	//Code
+	switch(iMsg)
 	{
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
+		case WM_DESTROY :
+			PostQuitMessage(0);
+			break;
 	}
-	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
-}
 
+	return (DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
