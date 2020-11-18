15a6eb4c9dc717c4c1b46ca6a749bad75a96152b
PhoeniX-d <prnv24choudhary@gmail.com>
2020-11-18 19:26:38 +0530

    Xaml Event handling

---
+++ WinRT/GUI/XamlEventHandling/MainPage.xaml.cpp
@@ -0,0 +1,45 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace XamlEventHandling;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+	Window::Current->CoreWindow->KeyDown += ref new TypedEventHandler<CoreWindow^, KeyEventArgs^>(this, &MainPage::OnKeyDown);
+}
+
+void MainPage::OnKeyDown(CoreWindow^ sender, KeyEventArgs^ args)
+{
+	/*if (args->VirtualKey == VirtualKey::Space)
+	{
+		tb->Text = "Space Bar is Pressed!";
+	}
+	else
+	{
+		tb->Text = "Some Key is Pressed!";
+	}*/
+}
+
+void MainPage::OnButtonClick(Object^ sender, RoutedEventArgs^ args)
+{
+	/*tb->Text = "Mouse is Clicked!";*/
+}
---
+++ WinRT/GUI/XamlEventHandling/MainPage.xaml.h
@@ -0,0 +1,30 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+#include "MainPage.g.h"
+#pragma once
+using namespace Platform;			/*for Object */
+using namespace Windows::UI;		/* for Colors */
+using namespace Windows::UI::Text;	/* for FontStyle */
+using namespace Windows::UI::Xaml;	 /*for Alignments */
+using namespace Windows::System;	/* for VirtualKey */
+using namespace Windows::UI::Xaml::Controls;	/* for Page, Grid */
+using namespace Windows::UI::Core;				 /* for CoreWindow and KeyEvenArgs */
+using namespace Windows::Foundation;			/* for TypedEventHandler*/
+using namespace Windows::UI::Xaml::Media; /* for SolidColorBrush*/
+
+
+namespace XamlEventHandling
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+		void OnKeyDown(CoreWindow^ sender, KeyEventArgs^ args);
+		void OnButtonClick(Object^ sender, RoutedEventArgs^ args);
+	};
+}
